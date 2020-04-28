5a346029e43d40ebd7deb7e0eaed2e309cd2fe85
zhengleixiong <lzhengnt@gmail.com>
2020-04-28 05:23:26 -0500

    Initial commit

---
+++ model/src/main/java/com/example/hoh/model/mapper/RecipeMapper.java
@@ -0,0 +1,24 @@
+package com.example.hoh.model.mapper;
+
+import com.example.hoh.model.entity.Recipe;
+
+import java.util.List;
+import java.util.Map;
+
+public interface RecipeMapper {
+    int deleteByPrimaryKey(Integer recipeId);
+
+    int insert(Recipe record);
+
+    int insertSelective(Recipe record);
+
+    Recipe selectByPrimaryKey(Integer recipeId);
+
+    List<Recipe> selectByCalories(Integer calories);
+
+    List<Recipe> selectByParamsMap(Map<String, Object> params);
+
+    int updateByPrimaryKeySelective(Recipe record);
+
+    int updateByPrimaryKey(Recipe record);
+}
